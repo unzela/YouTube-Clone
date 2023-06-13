692208bf517954fb4b78f2ad04bb7aa19e450fc3
Unzela Inamdar <uinamdar@microsoft.com>
2023-06-13 22:26:35 +0530

    Initial Commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className="w-44 p-5 shadow-lg">
+        <ul>
+        <li>Home</li>
+        <li>Shorts</li>
+        <li>Videos</li>
+        <li>Live</li>
+      </ul>
+      <h1 className="font-bold pt-2">Subscriptions</h1>
+      <ul>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+      </ul>
+      <h1 className="font-bold pt-5">Watch Later</h1>
+      <ul>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
