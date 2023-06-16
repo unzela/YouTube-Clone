d054f7e8e0f89b06c12b64dd527b9ca10e46a93e
Unzela Inamdar <uinamdar@microsoft.com>
2023-06-16 17:17:00 +0530

    Feature: Nested Comments

---
+++ src/components/CommentsContainer.js
@@ -0,0 +1,87 @@
+import React from 'react'
+
+const commentsData = [
+    {
+        name: "Unzela",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+        replies: [  
+            {
+            name: "Unzela",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+            replies: [ 
+                {
+                name: "Unzela",
+                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+                replies: []
+                },
+                {
+                    name: "Unzela",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+                    replies: [  
+                        {
+                        name: "Unzela",
+                        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+                        replies: []
+                        }
+                    ]
+                }
+            ]
+            }
+        ]
+    },
+    {
+        name: "Unzela",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+        replies: []
+    },
+    {
+        name: "Unzela",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+        replies: []
+    },
+    {
+        name: "Unzela",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+        replies: []
+    },
+    {
+        name: "Unzela",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+        replies: []
+    }
+]
+
+const Comment = ({data}) => {
+    const {name, text, replies} = data;
+    return(
+        <div className="flex shadow-sm p-2 bg-gray-100 rounded-lg my-2">
+            <img className="w-8 h-8" alt="user" src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png" />
+            <div className="px-3">
+                <p className="font-bold">{name}</p>
+                <p>{text}</p>
+            </div>
+        </div>
+    )
+}
+
+const CommentsList = ({comments}) => {
+    return comments.map((comment, index) => (
+        <div  key={index}>
+            <Comment data={comment} />
+            <div className="pl-5 border border-l-black ml-5">
+                <CommentsList comments={comment.replies}/>
+            </div>
+        </div>
+    ));
+}
+
+const CommentsContainer = () => {
+  return (
+    <div className="m-5 p-2">
+        <h1 className="text-2xl font-bold">Comments</h1>
+        <CommentsList comments={commentsData} />
+    </div>
+  )
+}
+
+export default CommentsContainer;
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,10 +2,10 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { closeMenu } from '../utils/appSlice';
 import { useSearchParams } from 'react-router-dom';
+import CommentsContainer from './CommentsContainer';
 
 const WatchPage = () => {
     const [searchParams] = useSearchParams();
-    console.log(searchParams.get("v"));
     
     const dispatch = useDispatch();
     useEffect(()=>{
@@ -13,6 +13,7 @@ const WatchPage = () => {
     },[]);
 
   return (
+    <div className="flex flex-col">
     <div className="px-5">
         <iframe 
             width="900" 
@@ -24,7 +25,9 @@ const WatchPage = () => {
             allowFullScreen>
         </iframe>
     </div>
+    <CommentsContainer />
+    </div>
   )
 }
 
-export default WatchPage
+export default WatchPage;
